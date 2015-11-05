7c534ec7c400d5a9dafc9cd031f23cec73937d9b
Florian Kugler <mail@floriankugler.com>
2015-11-05 17:40:56 +0100

    Improve output

--- pygments/formatters/objcio.py
+++ pygments/formatters/objcio.py
@@ -23,7 +23,8 @@ class ObjcioHtmlFormatter(HtmlFormatter):
     def __init__(self, **options):
         HtmlFormatter.__init__(self, **options)
         self.inputIndent = int(options.get('input-indent', '4'))
-        self.indent = int(options.get('indent', '3'))
+        self.indent = float(options.get('indent', '3'))
+        self.indentUnit = options.get('indent-unit', 'ch')
 
 
     def wrap(self, source, outfile):
@@ -31,11 +32,11 @@ class ObjcioHtmlFormatter(HtmlFormatter):
 
     def _wrap_code(self, source):
         yield 0, '<div class="code">'
-        format = '<div class="line" style="padding-left:{0}ch;margin-left:{2}ch;text-indent:-{2}ch;">{1}</div>'
+        format = '<div class="line" style="padding-left:{0}{3};margin-left:{2}{3};text-indent:-{2}{3};">{1}</div>'
         for i, t in source:
             if i == 1:
                 # it's a line of formatted code
                 trimmedLine = t.lstrip()
                 diff = len(t) - len(trimmedLine)
-                yield 1, format.format(self.indent * diff/self.inputIndent, trimmedLine, self.indent)
+                yield 1, format.format(self.indent * diff/self.inputIndent, trimmedLine, self.indent, self.indentUnit)
         yield 0, '</div>'
