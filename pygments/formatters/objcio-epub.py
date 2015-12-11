3afba3ace6a8cd2825a8b55ea4518734ebdab07d
Florian Kugler <mail@floriankugler.com>
2015-12-11 10:19:44 +0100

    Add custom formatter that include a code tag

---
+++ pygments/formatters/objcio-epub.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.formatters.html
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Formatter for HTML output.
+
+    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from pygments.formatters import HtmlFormatter
+
+__all__ = ['ObjcioEpubHtmlFormatter']
+
+
+class ObjcioEpubHtmlFormatter(HtmlFormatter):
+
+    name = 'objcio-epub'
+    aliases = []
+    filenames = ['*.html', '*.htm']
+
+    def __init__(self, **options):
+        HtmlFormatter.__init__(self, **options)
+        self.inputIndent = int(options.get('input-indent', '4'))
+        self.indent = float(options.get('indent', '3'))
+        self.indentUnit = options.get('indent-unit', 'ch')
+
+
+    def wrap(self, source, outfile):
+        return self._wrap_code(source)
+
+    def _wrap_code(self, source):
+        yield 0, '<div class="code">'
+        format = '<div class="line" style="padding-left:{0}{3};margin-left:{2}{3};text-indent:-{2}{3};">{1}</div>'
+        for i, t in source:
+            if i == 1:
+                # it's a line of formatted code
+                trimmedLine = t.lstrip()
+                diff = len(t) - len(trimmedLine)
+                yield 1, format.format(self.indent * diff/self.inputIndent, trimmedLine, self.indent, self.indentUnit)
+        yield 0, '</div>'
--- pygments/formatters/objcio.py
+++ pygments/formatters/objcio.py
@@ -16,27 +16,21 @@ __all__ = ['ObjcioHtmlFormatter']
 
 class ObjcioHtmlFormatter(HtmlFormatter):
 
-    name = 'OBJCIO'
-    aliases = ['objcio']
+    name = 'objcio'
+    aliases = []
     filenames = ['*.html', '*.htm']
 
     def __init__(self, **options):
         HtmlFormatter.__init__(self, **options)
-        self.inputIndent = int(options.get('input-indent', '4'))
-        self.indent = float(options.get('indent', '3'))
-        self.indentUnit = options.get('indent-unit', 'ch')
 
 
     def wrap(self, source, outfile):
         return self._wrap_code(source)
 
     def _wrap_code(self, source):
-        yield 0, '<div class="code">'
-        format = '<div class="line" style="padding-left:{0}{3};margin-left:{2}{3};text-indent:-{2}{3};">{1}</div>'
+        yield 0, '<div class="highlight"><pre><code>'
         for i, t in source:
             if i == 1:
                 # it's a line of formatted code
-                trimmedLine = t.lstrip()
-                diff = len(t) - len(trimmedLine)
-                yield 1, format.format(self.indent * diff/self.inputIndent, trimmedLine, self.indent, self.indentUnit)
-        yield 0, '</div>'
+                yield 1, t
+        yield 0, '</code></pre></div>'
